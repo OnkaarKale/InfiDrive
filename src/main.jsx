f0a99a9d27fd313ef16413813e87da041f9da758
OnkaarKale <onkaarkale@gmail.com>
2026-02-12 22:45:38 +0530

    Changed redirected url

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-west-1.amazonaws.com/us-west-1_ht7iWqYTA",
   client_id: "5o12fd61qhe56vbksajfqnlkg3",
-  redirect_uri: "http://localhost:5173",
+  redirect_uri: "https://infidrive.onkaarkale.workers.dev/",
   response_type: "code",
   scope: "email openid phone",
   onSigninCallback: () => {
